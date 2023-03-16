4bb79a7092660fbca60335161f8f53e087c48a13
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-03-16 14:16:50 +0100

    Creata lista e stampata com'è

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,23 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<String> wishList = new ArrayList<>();
+        boolean exit = false;
+        while(!exit){
+            System.out.println("Welcome to your wishlist, please write what you would like to receive");
+            String wish = scan.nextLine();
+            wishList.add(wish);
+            System.out.println("Great! So far you've added " + wishList.size() + " items to your wishlist");
+            System.out.println("Would you like to add more? (y/n)");
+            exit = !scan.nextLine().equalsIgnoreCase("y");
+        }
+
+        System.out.println(wishList);
+    }
+}
